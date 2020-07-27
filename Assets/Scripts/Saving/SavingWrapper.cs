2af40b712ee060473072b06d579eb3d2e8d02c37
stefan.duadsko <stefan.dudasko@triad.sk>
2020-07-27 12:13:46 +0200

    Prefab changes

---
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Saving;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Load()
+        {
+            // Call to saving system load
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+
+        private void Save()
+        {
+            // Call to saving system load
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+    }
+}
