d67b570696f0f4bb378922272c2d13663c3277b5
stefan.duadsko <stefan.dudasko@triad.sk>
2020-08-10 14:33:17 +0200

    projectile shoots at targer

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform target = null;
+    [SerializeField] float speed = 1;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null) return;
+
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+
+        if (targetCapsule == null) return target.position; // If target does not have capsule collider
+
+        return target.position + Vector3.up * targetCapsule.height / 2;
     }
 }
